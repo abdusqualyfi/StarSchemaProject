173a85e5c6b2457f97b6d6f19eb08f5c310383e4
abdus.choudhury@qualyfi.co.uk <abdus.choudhury@qualyfi.co.uk>
2023-01-31 14:29:14 +0000

    Bike Dimension table created

--- Abdus_1SchemaCreation.py.py
+++ Abdus_1SchemaCreation.py.py
@@ -1,49 +1,24 @@
 # Databricks notebook source
 import zipfile, subprocess, glob
 
+#Directories - ensure / is at the end
+main_folder = "/tmp/Abdus/"
+zip_location_folder ="/dbfs/tmp/landing/"
+zip_output_folder = "/dbfs/tmp/Abdus/landing"
+dbfs_directory = 'dbfs:/tmp/Abdus/landing/'
+
 #Delete everything in destination folder
-personal_landing_folder = "/tmp/Abdus/landing/"
-personal_bronze_folder = "/tmp/Abdus/Bronze/"
-dbutils.fs.rm(personal_landing_folder, True)
-dbutils.fs.rm(personal_bronze_folder, True)
+dbutils.fs.rm(main_folder, True)
 
 #Extract all zips in a given location
-landing_location_zips = "/dbfs/tmp/landing/*.zip"
+landing_location_zips = zip_location_folder + "*.zip"
 zip_files = glob.glob(landing_location_zips)
 for zip_file in zip_files:
-    extract_to_dir = "/dbfs/tmp/Abdus/landing"
+    extract_to_dir = zip_output_folder
     subprocess.call(["unzip", "-d", extract_to_dir, zip_file])
-
-#Individual extraction of Zip files
-#print(subprocess.call("unzip /dbfs/tmp/landing/trips.zip -d /dbfs/tmp/Abdus/landing", shell=True))
-#print(subprocess.call("unzip /dbfs/tmp/landing/riders.zip -d /dbfs/tmp/Abdus/landing", shell=True))
-#print(subprocess.call("unzip /dbfs/tmp/landing/stations.zip -d /dbfs/tmp/Abdus/landing", shell=True))
-#print(subprocess.call("unzip /dbfs/tmp/landing/payments.zip -d /dbfs/tmp/Abdus/landing", shell=True))
-
-#Move and Make Directories
-#dbutils.fs.mv('/tmp/Abdus/landing/stations.zip', '/tmp/landing/stations.zip')
-#dbutils.fs.mkdirs('/tmp/Abdus/Silver')
-#dbutils.fs.mkdirs('/tmp/Abdus/Gold')
-
-
-#Landing Directories
-landing_payments = 'dbfs:/tmp/landing/payments.zip'
-landing_riders = 'dbfs:/tmp/landing/riders.zip'
-landing_stations = 'dbfs:/tmp/landing/stations.zip'
-landing_trips = 'dbfs:/tmp/landing/trips.zip'
-
-#Unzip files to personal landing folder
-#with zipfile.ZipFile('/tmp/landing/payments.zip', "r") as zip_ref:
-#    zip_ref.extractall("dbfs:/tmp/Abdus/landing")
 
 #Check directory exists and has the extract csv files in it
 print(dbutils.fs.ls('/tmp/Abdus/landing'))
-
-#CSV file directories
-csv_payments = 'dbfs:/tmp/Abdus/landing/payments.csv'
-csv_riders = 'dbfs:/tmp/Abdus/landing/riders.csv'
-csv_stations = 'dbfs:/tmp/Abdus/landing/stations.csv'
-csv_trips = 'dbfs:/tmp/Abdus/landing/trips.csv'
 
 # COMMAND ----------
 
@@ -53,49 +28,50 @@ from pyspark.sql.types import *
 #Trip
 bronze_trip_header = ['trip_id', 'rideable_type', 'started_at', 'ended_at', 'start_station_id', 'end_station_id', 'rider_id']
 bronze_trip_schema = StructType([StructField(col, StringType(), True) for col in bronze_trip_header])
-bronze_trip_df = spark.read.option("header", "false").schema(bronze_trip_schema).csv(csv_trips)
+bronze_trip_df = spark.read.option("header", "false").schema(bronze_trip_schema).csv(dbfs_directory +  'trips.csv')
 #display(bronze_trip_df)
 
 #Payments
 bronze_payments_header = ['payment_id', 'date', 'amount', 'rider_id']
 bronze_payments_schema = StructType([StructField(col, StringType(), True) for col in bronze_payments_header])
-bronze_payments_df = spark.read.option("header", "false").schema(bronze_payments_schema).csv(csv_payments)
+bronze_payments_df = spark.read.option("header", "false").schema(bronze_payments_schema).csv(dbfs_directory +  'payments.csv')
 #display(bronze_payments_df)
 
 #Riders
-bronze_riders_header = ['rider_id', 'first', 'last', 'address', 'birthday', 'account_start', 'account_end', 'is_member']
+bronze_riders_header = ['rider_id', 'first', 'last', 'address', 'birthday', 'account_start', 'account_end', 'is_member'] 
 bronze_riders_schema = StructType([StructField(col, StringType(), True) for col in bronze_riders_header])
-bronze_riders_df = spark.read.option("header", "false").schema(bronze_riders_schema).csv(csv_riders)
+bronze_riders_df = spark.read.option("header", "false").schema(bronze_riders_schema).csv(dbfs_directory +  'riders.csv')
 #display(bronze_riders_df)
 
 #Stations
-bronze_stations_header = ['station_id', 'name', 'longitude', 'latitude']
+bronze_stations_header = ['station_id', 'name', 'longitude', 'latitude'] 
 bronze_stations_schema = StructType([StructField(col, StringType(), True) for col in bronze_stations_header])
-bronze_stations_df = spark.read.option("header", "false").schema(bronze_stations_schema).csv(csv_stations)
+bronze_stations_df = spark.read.option("header", "false").schema(bronze_stations_schema).csv(dbfs_directory +  'stations.csv')
 #display(bronze_stations_df)
 
 
 # COMMAND ----------
 
 #Save to delta in Bronze folder
-bronze_trip_df.write.format("delta").mode("overwrite").save("/tmp/Abdus/Bronze/trip")
-bronze_payments_df.write.format("delta").mode("overwrite").save("/tmp/Abdus/Bronze/payments")
-bronze_riders_df.write.format("delta").mode("overwrite").save("/tmp/Abdus/Bronze/riders")
-bronze_stations_df.write.format("delta").mode("overwrite").save("/tmp/Abdus/Bronze/stations")
+bronze_trip_df.write.format("delta").mode("overwrite").save(main_folder + "Bronze/trips")
+bronze_payments_df.write.format("delta").mode("overwrite").save(main_folder + "Bronze/payments")
+bronze_riders_df.write.format("delta").mode("overwrite").save(main_folder + "Bronze/riders")
+bronze_stations_df.write.format("delta").mode("overwrite").save(main_folder + "Bronze/stations")
+#########################################################################END OF SCHEMA CREATION#########################################################################
 
 # COMMAND ----------
 
 #Open Bronze folder delta
-bronze_to_silverdf_trip = spark.read.format("delta").load("/tmp/Abdus/Bronze/trip")
+bronze_to_silverdf_trip = spark.read.format("delta").load(main_folder + "Bronze/trips")
 display(bronze_to_silverdf_trip)
 
-bronze_to_silverdf_payments = spark.read.format("delta").load("/tmp/Abdus/Bronze/payments")
+bronze_to_silverdf_payments = spark.read.format("delta").load(main_folder + "Bronze/payments")
 #display(bronze_to_silverdf_payments)
 
-bronze_to_silverdf_riders = spark.read.format("delta").load("/tmp/Abdus/Bronze/riders")
+bronze_to_silverdf_riders = spark.read.format("delta").load(main_folder + "Bronze/riders")
 #display(bronze_to_silverdf_riders)
 
-bronze_to_silverdf_stations = spark.read.format("delta").load("/tmp/Abdus/Bronze/stations")
+bronze_to_silverdf_stations = spark.read.format("delta").load(main_folder + "Bronze/stations")
 #display(bronze_to_silverdf_stations)
 
 # COMMAND ----------
@@ -175,3 +151,45 @@ silver_riders_df.printSchema()
 
 print("Stations schema")
 silver_stations_df.printSchema()
+
+# COMMAND ----------
+
+#Save to delta in Silver folder
+silver_trips_df.write.format("delta").mode("overwrite").save(main_folder + "Silver/trips")
+silver_payments_df.write.format("delta").mode("overwrite").save(main_folder + "Silver/payments")
+silver_riders_df.write.format("delta").mode("overwrite").save(main_folder + "Silver/riders")
+silver_stations_df.write.format("delta").mode("overwrite").save(main_folder + "Silver/stations")
+#########################################################################END OF BRONZE NOTEBOOK#########################################################################
+
+# COMMAND ----------
+
+#Open Silver folder delta
+silver_to_golddf_trip = spark.read.format("delta").load(main_folder + "Silver/trips")
+display(silver_to_golddf_trip)
+
+silver_to_golddf_payments = spark.read.format("delta").load(main_folder + "Silver/payments")
+#display(silver_to_golddf_payments)
+
+silver_to_golddf_riders = spark.read.format("delta").load(main_folder + "Silver/riders")
+#display(silver_to_golddf_riders)
+
+silver_to_golddf_stations = spark.read.format("delta").load(main_folder + "Silver/stations")
+#display(silver_to_golddf_stations)
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+#Create dimension tables
+
+#Create Bike table
+bike_window = Window.orderBy("rideable_type")
+
+bike_df = silver_to_golddf_trip.select("rideable_type").distinct() \
+            .withColumn("bike_id", F.row_number().over(bike_window))  \
+            .select("bike_id", "rideable_type")
+
+display(bike_df)
+print(bike_df)
+
