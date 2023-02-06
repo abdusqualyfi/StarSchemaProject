581deb345e1878252940178305571ce27bdb1ba4
abdus.choudhury@qualyfi.co.uk <abdus.choudhury@qualyfi.co.uk>
2023-02-06 14:52:42 +0000

    adding automated tests

--- notebooks/N7_AutomatedTests.py
+++ notebooks/N7_AutomatedTests.py
@@ -4,18 +4,73 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC Asserts on Schema
+
+# COMMAND ----------
+
+# MAGIC %run /Repos/abdus.choudhury@qualyfi.co.uk/StarSchemaProject/notebooks/N0_SchemaCreation
+
+# COMMAND ----------
+
+assert gold_trips_df.schema == gold_trips_schema, "Schema mismatch on: Trips table"
+assert gold_payments_df.schema == gold_payments_schema, "Schema mismatch on: Payments table"
+assert gold_riders_df.schema == gold_riders_schema, "Schema mismatch on: Riders table"
+assert gold_stations_df.schema == gold_stations_schema, "Schema mismatch on: Stations table"
+assert gold_bikes_df.schema == gold_bikes_schema, "Schema mismatch on: Bikes table"
+assert gold_dates_df.schema == gold_dates_schema, "Schema mismatch on: Dates table"
+assert gold_times_df.schema == gold_times_schema, "Schema mismatch on: Times table"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Asserts on Business Outcomes
+
+# COMMAND ----------
+
 # MAGIC %run /Repos/abdus.choudhury@qualyfi.co.uk/StarSchemaProject/notebooks/N6_BusinessOutcomes
 
 # COMMAND ----------
 
-#Asserts
+from pyspark.sql.functions import col
 
-#Bike count
-assert gold_bikes_df.count() == 3, "Incorrect number of rows in bikes table"
+#Q1A) timeSpentPerRide_date
+assert timeSpentPerRide_date.count() == 7, "Incorrect number of rows in Q1A, there should only be 7 rows for 7 days"
 
-#Bike docked_bike check
-#assert gold_bikes_df[1] == "docked_bike", "Incorrect value in bikes table, expecting 'docked_bike'"
+#Q1B) timeSpentPerRide_time
+q1b_count = timeSpentPerRide_time.filter(col("trip_start_time") < 24).count()
+q1b_total = timeSpentPerRide_time.count()
+assert q1b_count == q1b_total, "Incorrect value found in Q1B, 23 should be the max value in this column"
+
+#Q1C) timeSpentPerRide_StartStation
+assert timeSpentPerRide_StartStation.count() == 74, "Incorrect number of rows in Q1C, expecting 74"
+
+#Q1D) timeSpentPerRide_EndStation
+assert timeSpentPerRide_EndStation.count() == 67, "Incorrect number of rows in Q1D, expecting 67"
+
+#Q1E) timeSpentPerRide_RiderAge
+q1e_count = timeSpentPerRide_RiderAge.filter(col("rider_age") < 5).count()
+assert q1e_count == 0, "Incorrect value found in Q1E, found rider younger than 5"
+
+#Q1F) timeSpentPerRide_RiderType
 
 
-#Deliberate fail check
-assert gold_dates_df.count() == 123, "Incorrect number of rows in dates table"
+#Q2A) moneySpentPerRide_perMonth
+
+
+#Q2B) moneySpentPerRide_perQuarter
+
+
+#Q2C) moneySpentPerRide_perYear
+
+
+#Q2D) moneySpentPerRiderAge
+
+
+#Q3A) memberAvgTripMonth
+
+
+#Q3B) memberAvgTripMinutes
+
+
+
